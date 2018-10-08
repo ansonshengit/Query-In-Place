1f040b096ef8209597b8ee247bc5cb5c3832fdae
Anson Shen <43723718+ansonshengit@users.noreply.github.com>
2018-10-08 18:24:43 +0800

    Create lambda_script.py

---
+++ scripts/lambda_script.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import json
+import boto3
+print('Loading function')
+
+def lambda_handler(event, context):
+#    print("Received event: " + json.dumps(event, indent=2))
+    message = event['Records'][0]['Sns']['Message']
+    parsed_message = json.loads(message)
+    
+    myjobId = parsed_message['JobId']
+    print("Original Message" + message)
+    print("/////////////Line Divider///////////////")
+    print(parsed_message)
+    print("/////////////Line Divider///////////////")
+    print("Job ID:" + myjobId)
+
+    if parsed_message['Action'] == "ArchiveRetrieval":
+        glacier = boto3.client("glacier")
+        s3 = boto3.client("s3")
+
+        response = glacier.get_job_output(
+        vaultName='vault1',
+        jobId=myjobId,
+        )
+
+        data = response['body'].read()
+
+        s3.put_object(Body=data, Bucket='ansons-us-east-1', Key='glacier-retrieve/get-job-output/airport-code-archive.csv')
+        print("put object submitted")
+    else :
+        print("not the right job type")
+  
