eabcb158e468aed7be774f3465ce63629e40228f
Anson Shen <43723718+ansonshengit@users.noreply.github.com>
2018-10-23 00:04:55 +0800

    Update glacier-select-large.py

--- scripts/glacier-select-large.py
+++ scripts/glacier-select-large.py
@@ -1,22 +1,23 @@
 import boto3
+# Define variables.
 glacier = boto3.client("glacier")
-vault_name = 'vault1'
-archive_id = '<your archive id>'
-output_bucket = '<your bucket>'
-output_bucket_prefix = '<your bucket prefix>'
+vault_name = 'builder0-vault1'
+archive_id = 'TnsNS4AF_Bo2VkQybTCQHcgoz1PbhKuQoPySP1wu8B_TTlKHkn9pjBizsTNvT0nv4mDGypAZLy36qitLaGWj7G45VyBw_oFR8OUHoIuJZuGfF7lUJh8Spwht4ddN6R7j4lGaOMcoYw'
+output_bucket = 'builder0-us-east-1'
+output_bucket_prefix = 'glacier-select'
 
+# Submit archive-retieval job for whole object:
 jobParameters = {
     "Type": "archive-retrieval",
     "ArchiveId": archive_id,
     "Tier": "Expedited"
         }
 
-
 print glacier.initiate_job(vaultName=vault_name, jobParameters=jobParameters)
 
 print ("////////Above is whole object retrieval, below is Glacier Select////////")
 
-
+# Submit Glacier select job to retrieve the subset of the object:
 jobParameters = {
     "Type": "select",
     "ArchiveId": archive_id,
@@ -37,6 +38,5 @@ jobParameters = {
         "S3": {"BucketName": output_bucket, "Prefix": output_bucket_prefix}
         }
 }
-
 
 print glacier.initiate_job(vaultName=vault_name, jobParameters=jobParameters)
